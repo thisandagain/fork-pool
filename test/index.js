4dbc8a99459c5540141c52f40a46c7a774400fb8
Andrew Sliwinski <thisandagain@gmail.com>
2012-08-03 12:55:18 -0700

    Updated tests

--- test/index.js
+++ test/index.js
@@ -21,20 +21,7 @@ async.auto({
         Pool.enqueue('hello', callback);
     },
 
-    stress:     function (callback) {
-        var load    = [];
-        var job     = function (callback) {
-            load.push(Pool.enqueue('hello', callback));
-        };
-
-        for (var i = 0; i < 20; i++) {
-            load.push(job);
-        }
-
-        async.parallel(load, callback);
-    },
-
-    test:   ['enqueue', 'stress', function (callback, obj) {
+    test:   ['enqueue', function (callback, obj) {
         test("Component definition", function (t) {
             t.type(Pool, "object", "Component should be an object");
             t.type(Pool.enqueue, "function", "Method should be a function");
@@ -45,12 +32,6 @@ async.auto({
         test("Enqueue", function (t) {
             t.type(obj.enqueue, "object", "Results should be an object");
             t.equal(obj.enqueue.stdout, "world", "Results should be expected response");
-            t.end();
-        });
-
-        test("Stress", function (t) {
-            t.type(obj.stress, "object", "Results should be an object");
-            t.equal(obj.stress[0].stdout, "world", "Results should be expected response");
             t.end();
         });
 
